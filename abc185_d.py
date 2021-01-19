53d4f0c3711bf624b8116df7b422dacfa380db82
lockdef <locktki@icloud.com>
2021-01-19 23:58:26 +0900

    2021/01/19 1AC

--- abc185_d.py
+++ abc185_d.py
@@ -1,2 +1,18 @@
+from math import ceil
 n, m = map(int, input().split())
 a = list(map(int, input().split()))
+a.append(n + 1)
+a.append(0)
+a.sort()
+d = []
+md = n
+for i in range(m + 1):
+    d_ = a[i + 1] - a[i] - 1
+    d.append(d_)
+    if d_ == 0:
+        continue
+    md = min(md, d_)
+c = 0
+for i in d:
+    c += ceil((i) / md)
+print(c)
